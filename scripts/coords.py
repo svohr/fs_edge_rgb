b9323120cdee466bc70a06b349ec9eea9985f6fb
svohr <shvohr@gmail.com>
2020-11-23 21:23:46 -0500

    Prototype generate lighting from image file

--- scripts/coords.py
+++ scripts/coords.py
@@ -1,24 +1,17 @@
 
+from PIL import Image
+
 
 rows = [
     ["hk0", "gap_half", "esc", "f1", "f2", "f3", "f4", "f5", "f6", "gap_double", "f7", "f8", "f9", "f10", "f11", "f12", "prnt", "pause", "del"],
-    ["hk1", "hk2", "gap_half", "tlde", "1", "2", "3", "4", "5", "6", "gap_double", "7", "8", "9", "0", "hyph", "=", "bspc", "home"],
+    ["hk1", "hk2", "gap_half", "tilde", "1", "2", "3", "4", "5", "6", "gap_double", "7", "8", "9", "0", "hyph", "=", "bspc", "home"],
     ["hk3", "hk4", "gap_half", "tab", "q", "w", "e", "r", "t", "gap_double", "y", "u", "i", "o", "p", "obrk", "cbrk", "\\", "end"],
     ["hk5", "hk6", "gap_half", "caps", "a", "s", "d", "f", "g", "gap_double", "h", "j", "k", "l", "colon", "apos", "ent", "pup"],
-    ["hk7", "hk8", "gap_half", "shft", "z", "x", "c", "v", "b", "gap_double", "n", "m", "com", "per", "/", "rshft", "up", "pdn"],
-    ["hk9", "hk10", "gap_half", "lctl", "lwin", "lalt", "lspc", "gap_single", "rspc", "ralt", "rctl", "lft", "dwn", "rght"],
+    ["hk7", "hk8", "gap_half", "lshft", "z", "x", "c", "v", "b", "gap_double", "n", "m", "com", "per", "/", "rshft", "up", "pdn"],
+    ["hk9", "hk10", "gap_half", "lctrl", "lwin", "lalt", "lspc", "gap_single", "rspc", "ralt", "rctrl", "lft", "dwn", "rght"],
 ]
 
-board_width = sum(key_size.get(k, 1.0) for k in rows)
-board_height = len(rows) + 0.5
-
 blanks = ["gap_half", "gap_single", "gap_double"]
-
-key_to_row = {
-    k: i for i, row in enumerate(rows) for k in rows[i] if k not in blanks
-}
-
-keys_all = [k for r in rows for k in r if k not in blanks]
 
 key_size = {
     "hk0": 2,
@@ -40,6 +33,15 @@ key_size = {
     "gap_double": 1.50,
 }
 
+board_width = sum(key_size.get(k, 1.0) for k in rows[0])
+board_height = len(rows) + 0.5
+
+key_to_row = {
+    k: i for i, row in enumerate(rows) for k in rows[i] if k not in blanks
+}
+
+keys_all = [k for r in rows for k in r if k not in blanks]
+
 
 def get_key_x_center(key):
     row = rows[key_to_row[key]]
@@ -58,10 +60,17 @@ def get_key_coords(key):
 
 
 def scale_coords(c, dims):
-    return ((c[0] / board_width) * dims[0], (c[1] / board_height) * dims[1])
+    return (int((c[0] / board_width) * dims[0]), int((c[1] / board_height) * dims[1]))
 
 
 def get_key_image_coords(k, dims):
     return scale_coords(get_key_coords(k), dims)
 
+
+im = Image.open("seabass.png")
+
+for k in keys_all:
+    coords = get_key_image_coords(k, im.size)
+    color = im.getpixel(coords)
+    print(f"[{k}]>[{color[0]}][{color[1]}][{color[2]}]")
 
